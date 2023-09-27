b7eaf0aaee60c8b8116202a5fa5f53af062e4982
GonzaloMartinez <gonzalo.martinez@codename64.com>
2023-09-27 09:32:19 +0200

    tesaa

--- next.config.js
+++ next.config.js
@@ -4,32 +4,5 @@ module.exports = {
     reactStrictMode: true,
     env: {
         TRADING_BOOK_API_ENDPOINT: process.env.TRADING_BOOK_API_ENDPOINT,
-    },
-    async headers() {
-        return [
-          {
-            // Routes this applies to
-            source: "/api/(.*)",
-            // Headers
-            headers: [
-              // Allow for specific domains to have access or * for all
-              {
-                key: "Access-Control-Allow-Origin",
-                value: "*",              
-              },
-              // Allows for specific methods accepted
-              {
-                key: "Access-Control-Allow-Methods",
-                value: "GET, POST, PUT, DELETE, OPTIONS",
-              },
-              // Allows for specific headers accepted (These are a few standard ones)
-              {
-                key: "Access-Control-Allow-Headers",
-                value: "Content-Type, Authorization",
-              },
-            ],
-          },
-        ];
-      }
-
+    }
 }
